803f43ed1a4eb80efc579922b67a46df6ee93592
manjotkaurmand <manjotkaurmand@student.mitt.ca>
2022-09-14 14:34:12 -0500

    one more commit for lab1

--- Lab1-API/Program.cs
+++ Lab1-API/Program.cs
@@ -1,3 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System.Text;
+
+String Concatenation(string wordArg, int numberArg)
+{
+    StringBuilder sb = new StringBuilder();
+
+ for(int j = 0; j < numberArg; j++)
+    {
+        sb.Append(wordArg);
+    }
+
+    return sb.ToString();
+}
+
+string mystring = "I have a Phobia from a Spider";
+
+stringDelegate mydel = new stringDelegate(Concatenation);
+Console.WriteLine(mydel(mystring, 3));//returning string thrice
+
+
+mydel = (string wordArg, int numberArg) =>
+{
+    //remove 8 charac
+
+    string removewrds = wordArg.Substring(0, wordArg.Length - numberArg);
+
+    return removewrds ;
+};
+
+Console.WriteLine(mydel("PHOTOELECTRIC", 8));
+
+delegate string stringDelegate(string wordArg, int numberArg);
+
 
